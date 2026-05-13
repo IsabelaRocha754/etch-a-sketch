6a353b08c985b09210ec9d0f92010b7c3aa09a70
Isabela <isarocha0012@gmail.com>
2026-05-13 19:38:05 -0300

    Erase previous grid
    Whenever you click grid size and choose a size, the precious grid is erased

--- javascript.js
+++ javascript.js
@@ -3,20 +3,29 @@ grid.classList.add("grid");
 document.body.appendChild(grid);
 grid.textContent = "Grid Size";
 
-const startOver = document.createElement("button");
-startOver.classList.add("startOver");
-document.body.appendChild(startOver);
-startOver.textContent = "Start Over";
-
 const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
-let gridSize = -1;
+for (let i = 0; i < 256; i++){
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+    square.style.width = `${800/16}px`;
+    square.style.height = `${800/16}px`;
+    square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "blue";
+    })
+}
+
 grid.addEventListener("click", () => {
+    let gridSize = -1;
+
     while (gridSize > 100 || gridSize < 0 || Number.isInteger(gridSize) == false){
         gridSize = Number(prompt("Grid Size: "));
     };
+
+    container.innerHTML = "";
 
     for (let i = 0; i < gridSize * gridSize; i++){
             const square = document.createElement("div");
@@ -28,5 +37,5 @@ grid.addEventListener("click", () => {
         })
         square.style.width = `${800 / (gridSize)}px`;
         square.style.height = `${800 / (gridSize)}px`;
-        };
+    };
 });
