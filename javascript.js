20296f7addd7f04b88f67446f81cf9bd03aaba0d
Isabela <isarocha0012@gmail.com>
2026-05-13 20:28:21 -0300

    Function to create grid and erase button
    Wrote a function that creates a grid by taking a parameter.
    The erase button works now.

--- javascript.js
+++ javascript.js
@@ -3,20 +3,16 @@ grid.classList.add("grid");
 document.body.appendChild(grid);
 grid.textContent = "Grid Size";
 
+const erase = document.createElement("button");
+erase.classList.add("erase");
+document.body.appendChild(erase);
+erase.textContent = "Erase";
+
 const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
-for (let i = 0; i < 256; i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
-    square.style.width = `${800/16}px`;
-    square.style.height = `${800/16}px`;
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "blue";
-    })
-}
+createGrid(16);
 
 grid.addEventListener("click", () => {
     let gridSize = -1;
@@ -25,17 +21,29 @@ grid.addEventListener("click", () => {
         gridSize = Number(prompt("Grid Size: "));
     };
 
+    createGrid(gridSize);
+
+    erase.addEventListener("click", () => {
+        createGrid(gridSize);
+    })
+});
+
+erase.addEventListener("click", () => {
+    createGrid(16);
+})
+
+function createGrid(size){
     container.innerHTML = "";
 
-    for (let i = 0; i < gridSize * gridSize; i++){
-            const square = document.createElement("div");
-            square.classList.add("square");
-            container.appendChild(square);
+    for (let i = 0; i < size * size; i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
 
-            square.addEventListener('mouseenter', () => {
+        square.addEventListener('mouseenter', () => {
             square.style.backgroundColor = 'blue';
-        })
-        square.style.width = `${800 / (gridSize)}px`;
-        square.style.height = `${800 / (gridSize)}px`;
-    };
-});
+        });
+        square.style.width = `${800 / size}px`;
+        square.style.height = `${800 / size}px`;
+    }
+};
