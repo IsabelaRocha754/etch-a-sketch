8e22a35e5633be605476ffc7d6668109ce69e350
Isabela <isarocha0012@gmail.com>
2026-05-13 20:47:16 -0300

    Random color function
    Create a function to generate a random rgb color.
    Added a button to choose the option to get rainbow colors instead of just blue.

--- javascript.js
+++ javascript.js
@@ -8,29 +8,40 @@ erase.classList.add("erase");
 document.body.appendChild(erase);
 erase.textContent = "Erase";
 
+const rainbow = document.createElement("button");
+rainbow.classList.add("rainbow");
+document.body.appendChild(rainbow);
+rainbow.textContent = "Rainbow";
+
 const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
+
+let colorful = false;
 
 createGrid(16);
 
 grid.addEventListener("click", () => {
     let gridSize = -1;
 
-    while (gridSize > 100 || gridSize < 0 || Number.isInteger(gridSize) == false){
-        gridSize = Number(prompt("Grid Size: "));
+    while (gridSize > 100 || gridSize <= 0 || Number.isInteger(gridSize) == false){
+        gridSize = Number(prompt("Grid Size (1-100): "));
     };
 
     createGrid(gridSize);
 
     erase.addEventListener("click", () => {
         createGrid(gridSize);
-    })
+    });
 });
 
 erase.addEventListener("click", () => {
     createGrid(16);
-})
+});
+
+rainbow.addEventListener("click", () => {
+    colorful = true;
+});
 
 function createGrid(size){
     container.innerHTML = "";
@@ -41,9 +52,22 @@ function createGrid(size){
         container.appendChild(square);
 
         square.addEventListener('mouseenter', () => {
-            square.style.backgroundColor = 'blue';
+            if (colorful == true){
+                square.style.backgroundColor = randomColor();
+            }
+            else{
+                square.style.backgroundColor = 'blue';
+            }
         });
         square.style.width = `${800 / size}px`;
         square.style.height = `${800 / size}px`;
-    }
+    };
+};
+
+function randomColor(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
 };
