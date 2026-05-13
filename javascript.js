6c224640d764985fbaa5dbd6799ba39bee1722fa
Isabela <isarocha0012@gmail.com>
2026-05-13 01:23:50 -0300

    Added the hover event
    When the mouse enters a square it turns blue, when the mouse leaves it goes back to white
    Added a border just to locate myself

--- javascript.js
+++ javascript.js
@@ -6,5 +6,10 @@ for (let i = 0; i < 256; i++){
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
-    square.textContent = "grid";
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'blue';
+    })
+    square.addEventListener('mouseleave', () => {
+        square.style.backgroundColor = 'white';
+    })
 };
